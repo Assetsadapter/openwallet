3c7100a991fcecb1ac390e991fc169d8feb9e210
leor <leor_cao@163.com>
2020-05-20 18:14:03 +0800

    mod：kto地址bug修正1.6.0版本

--- openw/account_manager.go
+++ openw/account_manager.go
@@ -16,164 +16,164 @@
 package openw
 
 import (
-	"fmt"
-	"strings"
-	"time"
-
-	"github.com/blocktree/openwallet/log"
-	"github.com/blocktree/openwallet/openwallet"
+    "fmt"
+    "strings"
+    "time"
+    
+    "github.com/blocktree/openwallet/log"
+    "github.com/blocktree/openwallet/openwallet"
 )
 
 // CreateAssetsAccount
 func (wm *WalletManager) CreateAssetsAccount(appID, walletID, password string, account *openwallet.AssetsAccount, otherOwnerKeys []string) (*openwallet.AssetsAccount, *openwallet.Address, error) {
-
-	var (
-		wallet *openwallet.Wallet
-	)
-
-	if len(account.Alias) == 0 {
-		return nil, nil, fmt.Errorf("account alias is empty")
-	}
-
-	if len(account.Symbol) == 0 {
-		return nil, nil, fmt.Errorf("account symbol is empty")
-	}
-
-	if account.Required == 0 {
-		account.Required = 1
-	}
-
-	symbolInfo, err := GetSymbolInfo(account.Symbol)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	wrapper, err := wm.NewWalletWrapper(appID, walletID)
-	if err == nil {
-		wallet = wrapper.GetWallet()
-	}
-
-	if account.IsTrust {
-
-		if wallet == nil {
-			return nil, nil, fmt.Errorf("wallet not exist")
-		}
-
-		log.Debugf("wallet[%v] is trusted", wallet.WalletID)
-		//使用私钥创建子账户
-		key, err := wrapper.HDKey(password)
-		if err != nil {
-			return nil, nil, err
-		}
-
-		newAccIndex := wallet.AccountIndex + 1
-
-		// root/n' , 使用强化方案
-		account.HDPath = fmt.Sprintf("%s/%d'", wallet.RootPath, newAccIndex)
-
-		childKey, err := key.DerivedKeyWithPath(account.HDPath, symbolInfo.CurveType())
-		if err != nil {
-			return nil, nil, err
-		}
-		account.PublicKey = childKey.GetPublicKey().OWEncode()
-		account.Index = uint64(newAccIndex)
-		account.AccountID = account.GetAccountID()
-
-		wallet.AccountIndex = newAccIndex
-	} else {
-
-		if wallet == nil {
-
-			//非托管的，创建资产账户的钱包
-			wallet, _, err = wm.CreateWallet(appID, &openwallet.Wallet{
-				Alias:    "imported",
-				WalletID: walletID,
-				IsTrust:  false,
-			})
-			if err != nil {
-				return nil, nil, err
-			}
-		}
-
-		wallet.AccountIndex = int(account.Index)
-	}
-
-	account.AddressIndex = -1
-
-	//组合拥有者
-	account.OwnerKeys = []string{
-		account.PublicKey,
-	}
-
-	for _, otherKey := range otherOwnerKeys {
-		if len(otherKey) > 0 {
-			account.OwnerKeys = append(account.OwnerKeys, otherKey)
-		}
-	}
-
-	if len(account.PublicKey) == 0 {
-		return nil, nil, fmt.Errorf("account publicKey is empty")
-	}
-
-	//保存钱包到本地应用数据库
-	db, err := wm.OpenDB(appID)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	tx, err := db.Begin(true)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	defer tx.Rollback()
-
-	err = tx.Save(wallet)
-	if err != nil {
-
-		return nil, nil, err
-	}
-
-	err = tx.Save(account)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	log.Debug("new account create success:", account.AccountID)
-
-	addresses, err := wm.CreateAddress(appID, walletID, account.GetAccountID(), 1)
-	if err != nil {
-		log.Debug("new address create failed, unexpected error:", err)
-	}
-
-	var addr *openwallet.Address
-	if len(addresses) > 0 {
-		addr = addresses[0]
-		account.AddressIndex++
-	}
-
-	return account, addr, nil
+    
+    var (
+        wallet *openwallet.Wallet
+    )
+    
+    if len(account.Alias) == 0 {
+        return nil, nil, fmt.Errorf("account alias is empty")
+    }
+    
+    if len(account.Symbol) == 0 {
+        return nil, nil, fmt.Errorf("account symbol is empty")
+    }
+    
+    if account.Required == 0 {
+        account.Required = 1
+    }
+    
+    symbolInfo, err := GetSymbolInfo(account.Symbol)
+    if err != nil {
+        return nil, nil, err
+    }
+    
+    wrapper, err := wm.NewWalletWrapper(appID, walletID)
+    if err == nil {
+        wallet = wrapper.GetWallet()
+    }
+    
+    if account.IsTrust {
+        
+        if wallet == nil {
+            return nil, nil, fmt.Errorf("wallet not exist")
+        }
+        
+        log.Debugf("wallet[%v] is trusted", wallet.WalletID)
+        //使用私钥创建子账户
+        key, err := wrapper.HDKey(password)
+        if err != nil {
+            return nil, nil, err
+        }
+        
+        newAccIndex := wallet.AccountIndex + 1
+        
+        // root/n' , 使用强化方案
+        account.HDPath = fmt.Sprintf("%s/%d'", wallet.RootPath, newAccIndex)
+        
+        childKey, err := key.DerivedKeyWithPath(account.HDPath, symbolInfo.CurveType())
+        if err != nil {
+            return nil, nil, err
+        }
+        account.PublicKey = childKey.GetPublicKey().OWEncode()
+        account.Index = uint64(newAccIndex)
+        account.AccountID = account.GetAccountID()
+        
+        wallet.AccountIndex = newAccIndex
+    } else {
+        
+        if wallet == nil {
+            
+            //非托管的，创建资产账户的钱包
+            wallet, _, err = wm.CreateWallet(appID, &openwallet.Wallet{
+                Alias:    "imported",
+                WalletID: walletID,
+                IsTrust:  false,
+            })
+            if err != nil {
+                return nil, nil, err
+            }
+        }
+        
+        wallet.AccountIndex = int(account.Index)
+    }
+    
+    account.AddressIndex = -1
+    
+    //组合拥有者
+    account.OwnerKeys = []string{
+        account.PublicKey,
+    }
+    
+    for _, otherKey := range otherOwnerKeys {
+        if len(otherKey) > 0 {
+            account.OwnerKeys = append(account.OwnerKeys, otherKey)
+        }
+    }
+    
+    if len(account.PublicKey) == 0 {
+        return nil, nil, fmt.Errorf("account publicKey is empty")
+    }
+    
+    //保存钱包到本地应用数据库
+    db, err := wm.OpenDB(appID)
+    if err != nil {
+        return nil, nil, err
+    }
+    
+    tx, err := db.Begin(true)
+    if err != nil {
+        return nil, nil, err
+    }
+    
+    defer tx.Rollback()
+    
+    err = tx.Save(wallet)
+    if err != nil {
+        
+        return nil, nil, err
+    }
+    
+    err = tx.Save(account)
+    if err != nil {
+        return nil, nil, err
+    }
+    
+    err = tx.Commit()
+    if err != nil {
+        return nil, nil, err
+    }
+    
+    log.Debug("new account create success:", account.AccountID)
+    
+    addresses, err := wm.CreateAddress(appID, walletID, account.GetAccountID(), 1)
+    if err != nil {
+        log.Debug("new address create failed, unexpected error:", err)
+    }
+    
+    var addr *openwallet.Address
+    if len(addresses) > 0 {
+        addr = addresses[0]
+        account.AddressIndex++
+    }
+    
+    return account, addr, nil
 }
 
 // GetAssetsAccountInfo
 func (wm *WalletManager) GetAssetsAccountInfo(appID, walletID, accountID string) (*openwallet.AssetsAccount, error) {
-
-	wrapper, err := wm.NewWalletWrapper(appID, "")
-	if err != nil {
-		return nil, err
-	}
-
-	account, err := wrapper.GetAssetsAccountInfo(accountID)
-	if err != nil {
-		return nil, err
-	}
-
-	return account, nil
+    
+    wrapper, err := wm.NewWalletWrapper(appID, "")
+    if err != nil {
+        return nil, err
+    }
+    
+    account, err := wrapper.GetAssetsAccountInfo(accountID)
+    if err != nil {
+        return nil, err
+    }
+    
+    return account, nil
 }
 
 //RefreshAssetsAccountBalance 刷新资产账户余额
@@ -246,229 +246,229 @@ func (wm *WalletManager) GetAssetsAccountInfo(appID, walletID, accountID string)
 
 // GetAssetsAccountList
 func (wm *WalletManager) GetAssetsAccountList(appID, walletID string, offset, limit int) ([]*openwallet.AssetsAccount, error) {
-
-	wrapper, err := wm.NewWalletWrapper(appID, "")
-	if err != nil {
-		return nil, err
-	}
-
-	accounts, err := wrapper.GetAssetsAccountList(offset, limit, "WalletID", walletID)
-	if err != nil {
-		return nil, err
-	}
-
-	return accounts, nil
+    
+    wrapper, err := wm.NewWalletWrapper(appID, "")
+    if err != nil {
+        return nil, err
+    }
+    
+    accounts, err := wrapper.GetAssetsAccountList(offset, limit, "WalletID", walletID)
+    if err != nil {
+        return nil, err
+    }
+    
+    return accounts, nil
 }
 
 // CreateAddress
 func (wm *WalletManager) CreateAddress(appID, walletID string, accountID string, count uint64) ([]*openwallet.Address, error) {
-
-	if count == 0 {
-		return nil, fmt.Errorf("create address count is zero")
-	}
-
-	wrapper, err := wm.NewWalletWrapper(appID, "")
-	if err != nil {
-		return nil, err
-	}
-
-	account, err := wrapper.GetAssetsAccountInfo(accountID)
-	if err != nil {
-		return nil, err
-	}
-
-	assetsMgr, err := GetAssetsAdapter(account.Symbol)
-	if err != nil {
-		return nil, err
-	}
-
-	addrs, err := openwallet.BatchCreateAddressByAccount(account, assetsMgr, int64(count), 20)
-	if err != nil {
-		return nil, err
-	}
-
-	//打开数据库
-	db, err := wrapper.OpenStormDB()
-	if err != nil {
-		return nil, err
-	}
-	defer wrapper.CloseDB()
-
-	tx, err := db.Begin(true)
-	if err != nil {
-		return nil, err
-	}
-
-	defer tx.Rollback()
-
-	for _, addr := range addrs {
-		err = tx.Save(addr)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	account.AddressIndex = account.AddressIndex + int(count)
-
-	err = tx.Save(account)
-	if err != nil {
-		return nil, err
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return nil, err
-	}
-
-	//addrs, err := wrapper.CreateAddress(accountID, count, assetsMgr.GetAddressDecode(), false, false)
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	//log.Debug("addrs:", addrs)
-	//导入地址到核心钱包中
-	//go func() {
-	//
-	//	inErr := assetsMgr.ImportWatchOnlyAddress(addrs...)
-	//	if err != nil {
-	//		log.Error("import watch only address failed， unexpected err:", inErr)
-	//	}
-	//}()
-
-	//导入新地址到区块扫描器
-	for _, address := range addrs {
-		key := wm.encodeSourceKey(appID, address.AccountID)
-		wm.AddAddressForBlockScan(address.Address, key)
-	}
-
-	//log.Debug("new addresses create success:", addrs)
-
-	return addrs, nil
+    
+    if count == 0 {
+        return nil, fmt.Errorf("create address count is zero")
+    }
+    
+    wrapper, err := wm.NewWalletWrapper(appID, "")
+    if err != nil {
+        return nil, err
+    }
+    
+    account, err := wrapper.GetAssetsAccountInfo(accountID)
+    if err != nil {
+        return nil, err
+    }
+    
+    assetsMgr, err := GetAssetsAdapter(account.Symbol)
+    if err != nil {
+        return nil, err
+    }
+    
+    addrs, finalIndex, err := openwallet.BatchCreateAddressByAccount(account, assetsMgr, int64(count), 20)
+    if err != nil {
+        return nil, err
+    }
+    
+    //打开数据库
+    db, err := wrapper.OpenStormDB()
+    if err != nil {
+        return nil, err
+    }
+    defer wrapper.CloseDB()
+    
+    tx, err := db.Begin(true)
+    if err != nil {
+        return nil, err
+    }
+    
+    defer tx.Rollback()
+    
+    for _, addr := range addrs {
+        err = tx.Save(addr)
+        if err != nil {
+            return nil, err
+        }
+    }
+    
+    account.AddressIndex = finalIndex
+    
+    err = tx.Save(account)
+    if err != nil {
+        return nil, err
+    }
+    
+    err = tx.Commit()
+    if err != nil {
+        return nil, err
+    }
+    
+    //addrs, err := wrapper.CreateAddress(accountID, count, assetsMgr.GetAddressDecode(), false, false)
+    //if err != nil {
+    //	return nil, err
+    //}
+    
+    //log.Debug("addrs:", addrs)
+    //导入地址到核心钱包中
+    //go func() {
+    //
+    //	inErr := assetsMgr.ImportWatchOnlyAddress(addrs...)
+    //	if err != nil {
+    //		log.Error("import watch only address failed， unexpected err:", inErr)
+    //	}
+    //}()
+    
+    //导入新地址到区块扫描器
+    for _, address := range addrs {
+        key := wm.encodeSourceKey(appID, address.AccountID)
+        wm.AddAddressForBlockScan(address.Address, key)
+    }
+    
+    //log.Debug("new addresses create success:", addrs)
+    
+    return addrs, nil
 }
 
 // GetAddressList
 func (wm *WalletManager) GetAddressList(appID, walletID, accountID string, offset, limit int, watchOnly bool) ([]*openwallet.Address, error) {
-
-	wrapper, err := wm.NewWalletWrapper(appID, "")
-	if err != nil {
-		return nil, err
-	}
-
-	addrs, err := wrapper.GetAddressList(offset, limit, "AccountID", accountID, "WatchOnly", watchOnly)
-	if err != nil {
-		return nil, err
-	}
-
-	//var addrs []*openwallet.Address
-	//err = db.Find("AccountID", accountID, &addrs, storm.Limit(limit), storm.Skip(offset))
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	return addrs, nil
+    
+    wrapper, err := wm.NewWalletWrapper(appID, "")
+    if err != nil {
+        return nil, err
+    }
+    
+    addrs, err := wrapper.GetAddressList(offset, limit, "AccountID", accountID, "WatchOnly", watchOnly)
+    if err != nil {
+        return nil, err
+    }
+    
+    //var addrs []*openwallet.Address
+    //err = db.Find("AccountID", accountID, &addrs, storm.Limit(limit), storm.Skip(offset))
+    //if err != nil {
+    //	return nil, err
+    //}
+    
+    return addrs, nil
 }
 
 // GetAddress 获取单个地址信息
 func (wm *WalletManager) GetAddress(appID, walletID, accountID, address string) (*openwallet.Address, error) {
-
-	wrapper, err := wm.NewWalletWrapper(appID, "")
-	if err != nil {
-		return nil, err
-	}
-
-	addr, err := wrapper.GetAddress(address)
-	if err != nil {
-		return nil, err
-	}
-
-	//var addrs []*openwallet.Address
-	//err = db.Find("AccountID", accountID, &addrs, storm.Limit(limit), storm.Skip(offset))
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	return addr, nil
+    
+    wrapper, err := wm.NewWalletWrapper(appID, "")
+    if err != nil {
+        return nil, err
+    }
+    
+    addr, err := wrapper.GetAddress(address)
+    if err != nil {
+        return nil, err
+    }
+    
+    //var addrs []*openwallet.Address
+    //err = db.Find("AccountID", accountID, &addrs, storm.Limit(limit), storm.Skip(offset))
+    //if err != nil {
+    //	return nil, err
+    //}
+    
+    return addr, nil
 }
 
 // ImportWatchOnlyAddress
 func (wm *WalletManager) ImportWatchOnlyAddress(appID, walletID, accountID string, addresses []*openwallet.Address) error {
-
-	account, err := wm.GetAssetsAccountInfo(appID, walletID, accountID)
-	if err != nil {
-		return err
-	}
-
-	//wrapper, err := wm.NewWalletWrapper(appID, "")
-	//if err != nil {
-	//	return err
-	//}
-
-	//导入观测地址
-	//err = wrapper.ImportWatchOnlyAddress(addresses...)
-	//if err != nil {
-	//	return err
-	//}
-
-	//assetsMgr, err := GetAssetsAdapter(account.Symbol)
-	//if err != nil {
-	//	return err
-	//}
-
-	//导入地址到核心钱包中
-	//go assetsMgr.ImportWatchOnlyAddress(addresses...)
-
-	//导入新地址到区块扫描器
-	//scanner := assetsMgr.GetBlockScanner()
-
-	createdAt := time.Now()
-
-	//保存钱包到本地应用数据库
-	db, err := wm.OpenDB(appID)
-	if err != nil {
-		return err
-	}
-
-	tx, err := db.Begin(true)
-	if err != nil {
-		return err
-	}
-
-	defer tx.Rollback()
-
-	//importAddrs := make([]openwallet.ImportAddress, 0)
-
-	for _, a := range addresses {
-		a.WatchOnly = true //观察地址
-		a.Symbol = strings.ToUpper(account.Symbol)
-		a.AccountID = account.AccountID
-		a.CreatedTime = createdAt.Unix()
-		err = tx.Save(a)
-		if err != nil {
-			return err
-		}
-
-		key := wm.encodeSourceKey(appID, a.AccountID)
-		wm.AddAddressForBlockScan(a.Address, key)
-
-		//记录要导入到核心钱包的地址
-		imported := openwallet.ImportAddress{
-			Address: *a,
-		}
-
-		err = tx.Save(&imported)
-		if err != nil {
-			return err
-		}
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
-
-	log.Debug("import addresses success count:", len(addresses))
-
-	return nil
+    
+    account, err := wm.GetAssetsAccountInfo(appID, walletID, accountID)
+    if err != nil {
+        return err
+    }
+    
+    //wrapper, err := wm.NewWalletWrapper(appID, "")
+    //if err != nil {
+    //	return err
+    //}
+    
+    //导入观测地址
+    //err = wrapper.ImportWatchOnlyAddress(addresses...)
+    //if err != nil {
+    //	return err
+    //}
+    
+    //assetsMgr, err := GetAssetsAdapter(account.Symbol)
+    //if err != nil {
+    //	return err
+    //}
+    
+    //导入地址到核心钱包中
+    //go assetsMgr.ImportWatchOnlyAddress(addresses...)
+    
+    //导入新地址到区块扫描器
+    //scanner := assetsMgr.GetBlockScanner()
+    
+    createdAt := time.Now()
+    
+    //保存钱包到本地应用数据库
+    db, err := wm.OpenDB(appID)
+    if err != nil {
+        return err
+    }
+    
+    tx, err := db.Begin(true)
+    if err != nil {
+        return err
+    }
+    
+    defer tx.Rollback()
+    
+    //importAddrs := make([]openwallet.ImportAddress, 0)
+    
+    for _, a := range addresses {
+        a.WatchOnly = true //观察地址
+        a.Symbol = strings.ToUpper(account.Symbol)
+        a.AccountID = account.AccountID
+        a.CreatedTime = createdAt.Unix()
+        err = tx.Save(a)
+        if err != nil {
+            return err
+        }
+        
+        key := wm.encodeSourceKey(appID, a.AccountID)
+        wm.AddAddressForBlockScan(a.Address, key)
+        
+        //记录要导入到核心钱包的地址
+        imported := openwallet.ImportAddress{
+            Address: *a,
+        }
+        
+        err = tx.Save(&imported)
+        if err != nil {
+            return err
+        }
+    }
+    
+    err = tx.Commit()
+    if err != nil {
+        return err
+    }
+    
+    log.Debug("import addresses success count:", len(addresses))
+    
+    return nil
 }
 
 // importNewAddressToCoreWallet 导入新地址到核心钱包
